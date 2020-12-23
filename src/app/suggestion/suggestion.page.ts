ee2b38165c5d6bb4f869debcc71098e386be1e02
Gaëtan Hervé <gaetan.herve@lilo.org>
2020-12-23 10:51:21 +0100

    working on storage

--- src/app/suggestion/suggestion.page.ts
+++ src/app/suggestion/suggestion.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Suggestion} from '../models/Suggestion.model';
 import {SuggestionsService} from '../services/suggestions.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {IonicStorageModule, Storage} from '@ionic/storage';
 
 
 @Component({
@@ -20,9 +21,12 @@ export class SuggestionPage implements OnInit {
 
   constructor(private suggService: SuggestionsService,
               private route: ActivatedRoute,
-              private router: Router) {}
+              private router: Router,
+              private storage: Storage) {}
 
   ngOnInit() {
+    // Don't forget to remove next line !!!
+    this.storage.clear();
     const paramToCheck = 'origin';
     this.route.queryParams.subscribe(params => {
       this.origin = params[paramToCheck];
@@ -35,17 +39,51 @@ export class SuggestionPage implements OnInit {
           a['$key'] = item.key;
           this.Suggestions.push(a as Suggestion);
         });
-        this.generateSuggestion();
+        this.manageSuggetionUnicity().then( (result) => {
+          console.log('promise result : ', result);
+          if (!result) {
+            this.generateSuggestion();
+          }
+        });
         this.generateTitle();
         this.generateImage();
+        this.getAllFavorites();
+
       });
     });
   }
+
+
 
   // Unused ?
   fetchSuggestions() {
     this.suggService.getSuggestionList().valueChanges().subscribe(res => {
       // console.log(res);
+    });
+  }
+
+  manageSuggetionUnicity(): Promise<boolean> {
+    let suggestionMade = false;
+    let dateUsed = false;
+    let originUsed = false;
+    return new Promise( (resolve, reject) => {
+      const date = new Date(Date.now());
+      this.storage.get('date').then((val) => {
+        console.log('date in storage : ', val);
+        if (val === this.formatDate(date)) {
+          dateUsed = true;
+          this.storage.get(this.origin).then( (suggestion) => {
+            console.log('suggestion in storage : ', suggestion);
+            this.suggestion = suggestion;
+            originUsed = true;
+          });
+        }
+        if (dateUsed && originUsed){
+          suggestionMade = true;
+        }
+        console.log('suggestionMade ? ', suggestionMade);
+        resolve(suggestionMade);
+      });
     });
   }
 
@@ -61,17 +99,34 @@ export class SuggestionPage implements OnInit {
   }
 
   generateSuggestion() {
+    const date = new Date(Date.now());
     let categorisedSuggestions = [];
     let filteredWithDate = [];
     let randomNumber: number;
-    this.formatDate();
     categorisedSuggestions = this.Suggestions.filter( elem => elem.category === this.origin);
-    filteredWithDate = categorisedSuggestions.filter( elem => elem.date === this.formatDate());
-    // console.log('categorisedSuggestions before filter : ', categorisedSuggestions);
+    filteredWithDate = categorisedSuggestions.filter( elem => elem.date === this.formatDate(date));
     categorisedSuggestions = filteredWithDate.length > 0 ? filteredWithDate : categorisedSuggestions.filter( elem => elem.date == null);
     randomNumber = Math.trunc(Math.random() * categorisedSuggestions.length);
     this.suggestion = categorisedSuggestions[randomNumber];
-    // console.log('categorisedSuggestions after filter : ', categorisedSuggestions);
+    this.updateStorage(date);
+  }
+
+  updateStorage(date: Date) {
+    let existingSuggestion: object;
+    let suggestionToAdd;
+    const formattedDate = this.formatDate(date);
+    /*this.storage.get(formattedDate).then( (suggestion) => {
+      console.log('suggestion in storage : ', suggestion);
+      if (suggestion !== null) {
+        existingSuggestion = suggestion;
+      }
+
+    }); */
+    // this.storage.set(formattedDate, {origin: this.origin, suggestion: this.suggestion});
+    this.storage.set(formattedDate, ['elem1', 'elem2']).then( res => {
+      console.log('result storage.set : ', res);
+    });
+    // this.storage.set(this.origin, this.suggestion);
   }
 
   generateTitle() {
@@ -99,11 +154,23 @@ export class SuggestionPage implements OnInit {
     });
   }
 
-  formatDate(): string {
+  formatDate(date: Date): string {
     let result: string;
-    const date = new Date(Date.now());
     result = date.getDate() + '/' + (date.getMonth() + 1);
     return result;
+  }
+
+  getAllFavorites(){
+    const items = [];
+    return new Promise(resolve => {
+      this.storage.forEach( (v, k) => {
+        console.log('value : ', v);
+        console.log('key: ', k);
+        items.push(v);
+      }).then( () => {
+        resolve(items);
+      });
+    });
   }
 
 }
